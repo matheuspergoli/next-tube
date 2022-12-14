e7f30d2cdec4e14e7d0d3ad6c1bb1f6e9a1e1065
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-14 19:10:23 -0300

    refactor: add the setup for react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
-export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+export default function App({ Component, pageProps }: AppProps) {
+	const [queryClient] = React.useState(() => new QueryClient())
 	const [supabaseClient] = React.useState(() => createBrowserSupabaseClient())
 
 	return (
-		<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-			<Header />
-			<Component {...pageProps} />
-		</SessionContextProvider>
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+					<Header />
+					<Component {...pageProps} />
+				</SessionContextProvider>
+			</Hydrate>
+		</QueryClientProvider>
 	)
 }
