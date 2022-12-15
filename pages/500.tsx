f4481b0d0afd617f6952c12153b52ed710ad49ff
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-15 19:04:03 -0300

    feat: add a custom error page 500

---
+++ pages/500.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+function PageError500() {
+	const router = useRouter()
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			router.push('/')
+		}, 3000)
+	}, [router])
+
+	return (
+		<main className='flex h-screen w-screen items-center justify-center'>
+			<h1 className='text-4xl font-bold'>
+				Parece que você tentou fazer o download de um arquivo muito pesado, e infelizmente não conseguimos
+				liberar ele pre você.
+			</h1>
+			<h2 className='text-2xl font-bold'>Iremos redirecionar você em instantes!</h2>
+		</main>
+	)
+}
+
+export default PageError500
