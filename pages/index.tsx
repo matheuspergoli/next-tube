4d6150da60a3f787786c565643cfee84f904dd8f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-15 18:14:50 -0300

    feat: add a download button to every video and a function to hit api url that will send the video

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useQuery } from 'react-query'
+import { useRouter } from 'next/router'
 import { MainContainer } from '../components/MainContainer'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
 function Home() {
+	const router = useRouter()
 	const supabase = useSupabaseClient()
 	const { data: videos } = useQuery('videos', async () => {
 		const { data, error } = await supabase.from('videos').select('*')
@@ -15,6 +17,10 @@ function Home() {
 			return data as Video[]
 		}
 	})
+
+	async function downloadVideo(url: string) {
+		router.push(`/api/download/${url.replace('/video/', '')}`)
+	}
 
 	return (
 		<>
@@ -27,7 +33,12 @@ function Home() {
 				)}
 				<section className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 					{videos?.map((video) => (
-						<article key={video.id}>
+						<article key={video.id} className='relative'>
+							<button
+								className='absolute right-0 top-0 bg-blue-500 px-2 py-1 text-sm font-semibold text-white hover:bg-blue-600'
+								onClick={() => downloadVideo(video.link)}>
+								Download
+							</button>
 							<Link href={video.link}>
 								<img src={video.thumb} alt={video.title} className='w-full' />
 								<h2 className='text-center font-semibold'>{video.title}</h2>
