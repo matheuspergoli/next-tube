a4be99796a6732a9f93b4400f94b79b84a647fed
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-15 18:41:16 -0300

    refactor: change the downloader to send audio only, change to the api response limit is now false

--- pages/api/download/[url].ts
+++ pages/api/download/[url].ts
@@ -6,7 +6,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 	console.log(url)
 
 	const info = await ytdl.getInfo(url as string)
-	const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'videoandaudio' })
+	const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' })
 
 	res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`)
 	res.setHeader('Content-Type', 'video/mp4')
@@ -15,7 +15,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
 export const config = {
 	api: {
-		responseLimit: '100mb'
+		responseLimit: false
 	}
 }
 
