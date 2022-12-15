df3c320c293a0ad8bee605fe2468e9b270171a40
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-15 13:28:53 -0300

    feat: add a youtube player to watch the video based on their id passed by params

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -5,6 +5,7 @@ import { MainContainer } from '../../components/MainContainer'
 
 function Video() {
 	const router = useRouter()
+	const id = router.query.id
 
 	return (
 		<>
@@ -12,12 +13,16 @@ function Video() {
 				<title>Video</title>
 			</Head>
 			<MainContainer>
-				<h1 className='text-4xl font-bold'>Em construção!</h1>
 				<button
 					onClick={() => router.push('/')}
-					className='mt-10 rounded-md bg-zinc-800 px-3 py-2 text-white'>
+					className='mb-10 rounded-md bg-zinc-800 px-3 py-2 text-white'>
 					Voltar
 				</button>
+				<iframe
+					src={`https://youtube.com/embed/${id}`}
+					allowFullScreen
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+					className='mx-auto mb-10 h-iframe-video w-full max-w-4xl'></iframe>
 			</MainContainer>
 		</>
 	)
