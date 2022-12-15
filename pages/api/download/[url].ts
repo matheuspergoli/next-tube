580d513554285c5e4b4a592109cbfaf1ce20d649
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-15 18:15:50 -0300

    feat: create a api route to download the videos from the main page using ytdl-core

---
+++ pages/api/download/[url].ts
@@ -0,0 +1,16 @@
+import ytdl from 'ytdl-core'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { url } = req.query as { url: string }
+	console.log(url)
+
+	const info = await ytdl.getInfo(url as string)
+	const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'videoandaudio' })
+
+	res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`)
+	res.setHeader('Content-Type', 'video/mp4')
+	ytdl(url, { format: format }).pipe(res)
+}
+
+export default handler
